671947d350a0df9394333a6b69dc2e0d0864ad77
ashwath-gowda777 <ashwathgowda216@gmail.com>
2023-12-13 16:00:41 +0530

    This is the driver file for credit card data

--- src/Assignment_2/driver.py
+++ src/Assignment_2/driver.py
@@ -24,21 +24,20 @@ credit_card_df.show()
 
 # Get the number of partitions
 num_partitions = credit_card_df.rdd.getNumPartitions()
-print("Number of partitions:", num_partitions)
+
 
 # Repartition the DataFrame
 repartitioned_df = credit_card_df.repartition(13)
 repartitioned_partitions = repartitioned_df.rdd.getNumPartitions()
-print("Number of repartitioned partitions:", repartitioned_partitions)
+
 
 # Reduce the partition size back to its original size
 back_to_og_credit_card_df = repartitioned_df.coalesce(num_partitions)
 back_num_partitions = back_to_og_credit_card_df.rdd.getNumPartitions()
-print("Number of back_to_og_partitions:", back_num_partitions)
+
 
 # Apply the UDF to create a new column 'masked_card_number'
 masked_credit_card_df = back_to_og_credit_card_df.withColumn("masked_card_number", mask_card_udf()("card_number"))
 
 # Show the DataFrame with masked card numbers and repartitioned data
-print("DataFrame with masked card numbers and repartitioned data:")
 masked_credit_card_df.show()
