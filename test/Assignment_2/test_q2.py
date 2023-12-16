805c025346f462329a10cc9d9f805f3406b29ba3
ashwath-gowda777 <ashwathgowda216@gmail.com>
2023-12-16 11:36:09 +0530

    This is the unittest file for CeditCardData

--- test/Assignment_2/test_q2.py
+++ test/Assignment_2/test_q2.py
@@ -2,25 +2,25 @@ import unittest
 from pyspark.sql import SparkSession
 from PysparkRepo.src.Assignment_2.utils import *
 
-class TestMaskCardNumber(unittest.TestCase):
+class TestUtils(unittest.TestCase):
+
     def setUp(self):
-        self.spark = SparkSession.builder.appName("TestMaskCardNumber").getOrCreate()
+        self.spark = SparkSession.builder.appName("UnitTest").getOrCreate()
 
-    def test_mask_card_number(self):
-        test_cases = {
-            "1234567891234567": "************4567",
-            "5678912345671234": "************1234",
-            "9123456712345678": "************5678",
-            "1234567812341122": "************1122",
-            "1234": "1234"
-        }
+    def test_create_credit_card_dataframe(self):
+        data = [("1234567891234567",), ("5678912345671234",)]
+        schema = ["card_number"]
+        df = create_credit_card_dataframe(self.spark, data, schema)
+        self.assertEqual(df.count(), 2)
 
-        for card_number, expected_masked in test_cases.items():
-            masked = mask_card_number(card_number)
-            self.assertEqual(masked, expected_masked)
+    def test_increase_partitions(self):
+        data = [("1234567891234567",), ("5678912345671234",)]
+        schema = ["card_number"]
+        df = create_credit_card_dataframe(self.spark, data, schema)
+        increased_df = increase_partitions(df, 5)
+        self.assertEqual(increased_df.rdd.getNumPartitions(), 5)
 
-    def tearDown(self):
-        self.spark.stop()
+
 
 if __name__ == '__main__':
     unittest.main()
